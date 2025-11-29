3a0a5af58357c0f9e4a0773c2015b10ddba78720
David Khudaverdyan <134926510+eternal-crave@users.noreply.github.com>
2025-11-29 15:31:26 +0400

    Easy message hub extensions

---
+++ Assets/_Project/Scripts/Runtime/Helpers/MessageHubExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using R3;
+using R3.Triggers;
+using Easy.MessageHub;
+using Assets.Scripts.Infrastructure;
+
+namespace Assets.Scripts.Infrastructure.EasyMessageHub
+{
+    public static class MessageHubExtensions
+    {
+        public static void UnsubscribeWithOnDisable(this IMessageHub hub, MonoBehaviour toDisposeWith, Guid subscriberToken)
+        {
+            if (hub == null)
+                throw new ArgumentNullException(nameof(hub));
+
+            if (subscriberToken == null || subscriberToken == Guid.Empty)
+                throw new ArgumentNullException(nameof(subscriberToken));
+
+            if (toDisposeWith == null)
+                throw new ArgumentNullException(nameof(toDisposeWith));
+
+            toDisposeWith.OnDisableAsObservable().Subscribe(_ =>
+            {
+                if (hub.IsSubscribed(subscriberToken))
+                {
+                    hub.Unsubscribe(subscriberToken);
+                    Debug.Log($"Unsubscribed {subscriberToken} from {toDisposeWith.name} on Disable.");
+                }
+            });
+        }
+
+        public static void UnsubscribeWithOnDestroy(this IMessageHub hub, MonoBehaviour toDisposeWith, Guid subscriberToken)
+        {
+            if (hub == null)
+                throw new ArgumentNullException(nameof(hub));
+
+            if (subscriberToken == null || subscriberToken == Guid.Empty)
+                throw new ArgumentNullException(nameof(subscriberToken));
+
+            if (toDisposeWith == null)
+                throw new ArgumentNullException(nameof(toDisposeWith));
+
+            toDisposeWith.OnDestroyAsObservable().Subscribe(_ =>
+            {
+                if (hub.IsSubscribed(subscriberToken))
+                {
+                    hub.Unsubscribe(subscriberToken);
+                    Debug.Log($"Unsubscribed {subscriberToken} from {toDisposeWith.name} on destroy.");
+                }
+            });
+        }
+
+        public static void SubscribeSafe<T>(this IMessageHub hub, MonoBehaviour toDisposeWith, Action<T> action, DisposeMethod unsubscribeOnDestroy = DisposeMethod.OnDestroy)
+        {
+            if (hub == null)
+                throw new ArgumentNullException(nameof(hub));
+
+            if (toDisposeWith == null)
+                throw new ArgumentNullException(nameof(toDisposeWith));
+
+            if (action == null)
+                return;
+
+            var subscriberToken = hub.Subscribe(action);
+
+            if (unsubscribeOnDestroy == DisposeMethod.OnDestroy)
+            {
+                hub.UnsubscribeWithOnDestroy(toDisposeWith, subscriberToken);
+                return;
+            }
+
+            hub.UnsubscribeWithOnDisable(toDisposeWith, subscriberToken);
+        }
+
+        //in future get this working with non-mono classes
+    }
+}
